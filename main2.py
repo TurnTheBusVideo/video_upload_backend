f9d6f36fd860887f7480f6954d0130262ca26077
annsarapaul <annsarapaul@gmail.com>
2020-01-25 18:00:17 -0800

    Code to upload from S3

--- main2.py
+++ main2.py
@@ -12,7 +12,8 @@
 #    https://developers.google.com/api-client-library/python/guide/media_upload
 
 import os
-
+import boto3
+import json
 # import google_auth_oauthlib.flow
 from googleapiclient import discovery
 # import googleapiclient.errors
@@ -27,61 +28,84 @@ scopes = ["https://www.googleapis.com/auth/youtube.upload","https://www.googleap
 
 
 def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
+    ## Supports only mp4 Files
+    #Variables
+    BUCKET_NAME = 'test-turnthebus-upload'
+    OBJECT_KEY = 'TestYoutube/Test_API.mp4'
+    TEMP_FILE = 'Temp_S3File.mp4'
+    VIDEO_TITLE = 'Test'
+    VIDEO_DESCRIPTION = 'Awesome'
+    VIDEO_CHANNEL = 'UCWuYgDOn2z66ZnUNmCTP0ig'
+    TAGS = ["S3", "Test"]
+
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     api_service_name = "youtube"
     api_version = "v3"
-    #client_secrets_file = "client_secret_824525138655-6c49ppcebhlmqp9s5nfq6u2nc62p2t9q.apps.googleusercontent.com.json"
-    # client_secrets_file = "client_secret_824525138655-dhps58m377m2rejan9d2rfm7cl5gejag.apps.googleusercontent.com.json"
     SERVICE_ACCOUNT_FILE = "videopipeline-71f56f9de636.json"
-    # Get credentials and create an API client
-    # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-    #     client_secrets_file, scopes)
-    # credentials = flow.run_console()
-
     credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=scopes)
     delegated_credentials = credentials.with_subject('ann@turnthebus.org')
-
     youtube = discovery.build(
         api_service_name, api_version, credentials=delegated_credentials)
+    # Code for S3
+    
+    
 
-    request = youtube.videos().insert(
-        part="snippet, status",
-        onBehalfOfContentOwnerChannel = "UCWuYgDOn2z66ZnUNmCTP0ig",
-        onBehalfOfContentOwner = "ann@turnthebus.org",
-        #onBehalfOfContentOwner = "WuYgDOn2z66ZnUNmCTP0ig",
-        #onBehalfOfContentOwner = "c4rG0MP16xnAhMRJ4UWxTw",
-        #onBehalfOfContentOwnerChannel = "UCc4rG0MP16xnAhMRJ4UWxTw",
-        body={
-          "snippet": {
-            "title": "Test",
-            "description": "Awesome",
-            "categoryId": "22",
-            "channelId": "UCWuYgDOn2z66ZnUNmCTP0ig",
-            #"channelId": "UCc4rG0MP16xnAhMRJ4UWxTw",
-            #"onBehalfOfContentOwner" : "113851783885755510658",
-            #"onBehalfOfContentOwner" : "c4rG0MP16xnAhMRJ4UWxTw",
-            #"onBehalfOfContentOwnerChannel" : "UCc4rG0MP16xnAhMRJ4UWxTw",
 
-            "tags": [
-              "Test"
-            ]
-          },
-          "status": {
-            "privacyStatus": "public"
-          }
-        },
-        #file = "Test_API.mp4",
+    S3 = boto3.resource('s3')
+    s3_client = boto3.client('s3')
+    
+    if OBJECT_KEY[-4:].lower() == ".mp4":
+        s3_client.download_file(BUCKET_NAME, OBJECT_KEY, TEMP_FILE)
+    # End of Code for S3
+        media = MediaFileUpload(TEMP_FILE,resumable=True)
+        request = youtube.videos().insert(
+            part="snippet, status",
+            #onBehalfOfContentOwnerChannel = "UCWuYgDOn2z66ZnUNmCTP0ig",
+            #onBehalfOfContentOwner = "ann@turnthebus.org",
+            #onBehalfOfContentOwner = "WuYgDOn2z66ZnUNmCTP0ig",
+            #onBehalfOfContentOwner = "c4rG0MP16xnAhMRJ4UWxTw",
+            #onBehalfOfContentOwnerChannel = "UCc4rG0MP16xnAhMRJ4UWxTw",
+            body={
+              "snippet": {
+                "title": VIDEO_TITLE,
+                "description": VIDEO_DESCRIPTION,
+                "categoryId": "22",
+                "channelId": VIDEO_CHANNEL,
+                #"channelId": "UCc4rG0MP16xnAhMRJ4UWxTw",
+                "tags": TAGS
+              },
+              "status": {
+                "privacyStatus": "public"
+              }
+            },
+
+            #media_body=MediaFileUpload("Test_API.mp4")
+            media_body = media
+        )
         
-        # TODO: For this request to work, you must replace "YOUR_FILE"
-        #       with a pointer to the actual file you are uploading.
-        media_body=MediaFileUpload("Test_API.mp4")
-    )
-    response = request.execute()
+        response = None
+        while response is None:
+            status, response = request.next_chunk()
+            if status:
+                print "Uploaded %d%%." % int(status.progress() * 100)
+            print "Upload Complete!"
+        
+        #response = request.execute()
+        
+        #Add code to delete File after upload
+        #print(response)
+        return {
+            'statusCode': 200,
+            'body': json.dumps('File ' + OBJECT_KEY + ' Uploaded')
+        }
+    else:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('File ' + OBJECT_KEY + ' is not in mp4')
+        }
 
-    print(response)
 
 if __name__ == "__main__":
-    main()
+    z = main()
+    print(z)
