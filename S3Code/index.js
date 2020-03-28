fa99e2b553184904c34b0438fa951a08828788f1
Prasad Thakur <prasad.thakur@sap.com>
2020-03-28 03:35:36 -0700

    added dynamoDB fields

--- S3Code/index.js
+++ S3Code/index.js
@@ -2,66 +2,94 @@ const { uuid } = require('uuidv4');
 const { putIntoDynamo } = require('./dynamo');
 const { getSignedUrl } = require('./getSignedUrl');
 
-exports.handler = 
+exports.handler =
 
-async (event) => {
-  let responseCode = 200;
-  console.log("request: " + JSON.stringify(event));
-  
-  if (event.headers) {
+  async (event) => {
+    let responseCode = 200;
+    console.log("request: " + JSON.stringify(event));
+
+    if (event.headers) {
       console.log("Received headers: " + event.headers);
-  }
+    }
 
-  const fileName = event.queryStringParameters.key;
+    const fileName = event.queryStringParameters.key;
 
-  const { bucket,
-    course, 
-    section, 
-    subsection, 
-    notes } = event.queryStringParameters;
+    const {
+      bucket,
+      classN,
+      stream,
+      board,
+      bookName,
+      bookPartName,
+      chapterName,
+      chapterNumber,
+      chapterPart,
+      section,
+      title,
+      description,
+      videoLanguage,
+      awsRegion
+    } = event.queryStringParameters;
 
-  const signedURL = getSignedUrl(fileName, bucket);
+    const signedURL = getSignedUrl(fileName, bucket);
 
-  const data = { fileName, course, section, subsection, notes, uploadID: uuid(), 
-    S3URL: `https://${bucket}.s3.amazonaws.com/${fileName}` };
+    const data = {
+      fileName,
+      uploadID: uuid(),
+      AWS_REGION: awsRegion,
+      s3URL: `https://${bucket}.s3.amazonaws.com/${fileName}`,
+      Bucket: bucket,
+      classN,
+      stream,
+      board,
+      bookName,
+      bookPartName,
+      chapterName,
+      chapterNumber,
+      chapterPart,
+      section,
+      videoTitle: title,
+      videoDescription: description,
+      videoLanguage
+    };
 
-  console.log(JSON.stringify(data));
+    console.log(JSON.stringify(data));
 
-  await putIntoDynamo('UploadVideo', data);
+    await putIntoDynamo('UploadVideo', data);
 
-  console.log(JSON.stringify(data));
+    console.log(JSON.stringify(data));
 
-  // const args = {
-  //   BUCKET_NAME: bucket,
-  //   OBJECT_KEY: fileName,
-  //   TEMP_FILE: 'Temp_S3File.mp4',
-  //   VIDEO_TITLE: 'Test',
-  //   VIDEO_DESCRIPTION: 'Awesome',
-  //   VIDEO_CHANNEL: 'UCWuYgDOn2z66ZnUNmCTP0ig',
-  //   TAGS: ["S3", "Test"]
-  // };
+    // const args = {
+    //   BUCKET_NAME: bucket,
+    //   OBJECT_KEY: fileName,
+    //   TEMP_FILE: 'Temp_S3File.mp4',
+    //   VIDEO_TITLE: 'Test',
+    //   VIDEO_DESCRIPTION: 'Awesome',
+    //   VIDEO_CHANNEL: 'UCWuYgDOn2z66ZnUNmCTP0ig',
+    //   TAGS: ["S3", "Test"]
+    // };
 
-  // const result = await invokeLambda({
-  //   FunctionName: 'S3toYoutube',
-  //   Payload: JSON.stringify(args),
-  // });
+    // const result = await invokeLambda({
+    //   FunctionName: 'S3toYoutube',
+    //   Payload: JSON.stringify(args),
+    // });
 
-  const responseBody = {
-    signedURL: signedURL,
-  };
+    const responseBody = {
+      signedURL: signedURL,
+    };
 
-  const response = {
+    const response = {
       statusCode: responseCode,
       body: JSON.stringify(responseBody),
       headers: {
-        'Content-Type': 'application/json', 
+        'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': '*'
       }
+    };
+    console.log("response: " + JSON.stringify(response))
+    return response;
   };
-  console.log("response: " + JSON.stringify(response))
-  return response;
-};
 
 
 
