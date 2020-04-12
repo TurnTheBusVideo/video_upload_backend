844dfda0b8e2e57c98dc5fa0a6d104440be50840
Prasad Thakur <prasad.thakur@sap.com>
2020-04-12 02:01:06 -0700

    refactor 🧹

--- S3Code/index.js
+++ S3Code/index.js
@@ -16,64 +16,7 @@ exports.handler =
     const fileExtension = splitFileNameArry.pop();
     const fileName = name + '_' + Date.now() + '.' + fileExtension;
 
-    const {
-      bucket,
-      classN,
-      stream,
-      board,
-      bookName,
-      bookPartName,
-      chapterName,
-      chapterNumber,
-      chapterPart,
-      section,
-      tags,
-      title,
-      description,
-      videoLanguage,
-      awsRegion
-    } = event.queryStringParameters;
-
     const signedURL = getSignedUrl(fileName, bucket);
-
-    const data = {
-      fileName,
-      uploadID: fileName,
-      AWS_REGION: awsRegion,
-      s3URL: `https://${bucket}.s3.amazonaws.com/${fileName}`,
-      Bucket: bucket,
-      classN,
-      stream,
-      board,
-      bookName,
-      bookPartName,
-      chapterName,
-      chapterNumber,
-      chapterPart,
-      section,
-      videoTitle: title,
-      videoDescription: description,
-      videoLanguage,
-      Tags: tags.split(',')
-    };
-
-    console.log(JSON.stringify(data));
-
-
-    // const args = {
-    //   BUCKET_NAME: bucket,
-    //   OBJECT_KEY: fileName,
-    //   TEMP_FILE: 'Temp_S3File.mp4',
-    //   VIDEO_TITLE: 'Test',
-    //   VIDEO_DESCRIPTION: 'Awesome',
-    //   VIDEO_CHANNEL: 'UCWuYgDOn2z66ZnUNmCTP0ig',
-    //   TAGS: ["S3", "Test"]
-    // };
-
-    // const result = await invokeLambda({
-    //   FunctionName: 'S3toYoutube',
-    //   Payload: JSON.stringify(args),
-    // });
 
     const responseBody = {
       signedURL: signedURL,
