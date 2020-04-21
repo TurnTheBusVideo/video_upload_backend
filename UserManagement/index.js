ccb4676caa864f36019c4e0d105e7a713c9a17a7
Prasad Thakur <prasad.thakur@sap.com>
2020-04-21 15:25:50 -0700

    New user sign-up email to mods 📬

--- UserManagement/index.js
+++ UserManagement/index.js
@@ -1,22 +1,24 @@
 const AWS = require('aws-sdk');
 const fs = require('fs');
 
-
-let rawConfig = fs.readFileSync('config.json');
-let jsonConfig = JSON.parse(rawConfig);
+const configFile = fs.readFileSync('config.json');
+const configObj = JSON.parse(configFile);
+const moderators = configObj['mods']; //array of mod emails
 
 const cognitoIDP = new AWS.CognitoIdentityServiceProvider({
-    accessKeyId: jsonConfig.accessKeyId,
-    secretAccessKey: jsonConfig.secretAccessKey,
+    accessKeyId: configObj.accessKeyId,
+    secretAccessKey: configObj.secretAccessKey,
     region: 'ap-south-1'
 });
-
+const ses = new AWS.SES();
 
 exports.handler = (event, context, callback) => {
+    console.log('Version 🍫'); // for CloudWatch readability
     console.log('event details', JSON.stringify(event, null, 2));
     const { userName, userPoolId } = event;
     if (cognitoIDP && userName && userPoolId) {
         disableUser(userName, userPoolId, (status) => {
+            console.log(status);
             callback(null, event);
         })
     }
@@ -25,8 +27,8 @@ exports.handler = (event, context, callback) => {
     }
 };
 
-const disableUser = (userName, userPoolId, wrappedCallback) => {
-    console.log(`Trying to disable user: ${userName}, ${userPoolId}`);
+const disableUser = (userName, userPoolId, callback) => {
+    console.log(`===TRYING TO DISABLE USER ${userName}, ${userPoolId} ==`);
     var params = {
         UserPoolId: userPoolId,
         Username: userName
@@ -34,9 +36,47 @@ const disableUser = (userName, userPoolId, wrappedCallback) => {
     cognitoIDP.adminDisableUser(params, (err) => {
         if (err) {
             console.log(err);
+            callback('⚠️ User not disabled, email not sent to moderators!');
         } else {
-            console.log("===USER DISABLED===");
+            console.log(" ✅ USER DISABLED ");
+            sendEmail(moderators, userName, callback)
         }
-        wrappedCallback('User Disabled');
     });
 }
+
+function sendEmail(to, userName, callback) {
+    console.log("===TRYING TO MAIL MODERATORS===", to);
+    var emailParams = {
+        Destination: {
+            ToAddresses: to
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data:
+                    `A new user (${userName}) has registered on the Turn the Bus video portal.
+Their account is disabled until explicitly enabled.
+As a moderator you're expected to review the new user and enable their account.
+If you wish to enable this account, please login to the AWS console and navigate to
+Cognito IDP > Manage User Pools > content_creators > Users and Groups > Username (${userName}) > Enable User
+                    `
+                }
+            },
+            Subject: {
+                Data: "Turn the Bus | Video Upload Portal | New User"
+            }
+        },
+        // Replace source_email with your SES validated email address
+        Source: 'noreply@turnthebus.org'
+    };
+
+    ses.sendEmail(emailParams, function(err, data){
+        if (err) {
+            console.log(err);
+            callback('⚠️ User Disabled, failed to send email to moderators!');
+        } else {
+            console.log("✅ EMAIL SENT");
+            callback('✅ User Disabled, email sent to moderators.');
+        }
+    });
+};
